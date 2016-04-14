4aae3d29918c96d5fba94575b9e1d052027d694a
FELIPE GOUVEA DA SILVA CARVALHO <95felipeg@gmail.com>
2016-04-14 23:47:50 +0000

    with reader done

--- text-editor/main.c
+++ text-editor/main.c
@@ -3,81 +3,117 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
-#define max_line 4
-#define N 10000
+#define max_line 42
+#define N 10000000
 pthread_mutex_t lock_buffer;
 char buffer[N];
 sem_t vazio; // <- N
 sem_t cheio; // <- 0
 int in = 0,  //posicao p/ inserir no buffer itens
     out= 0;  //posicao p/ remover do buffer itens
-
+long int it;
 void *processador() {
 
 	while (1) {
 		sem_wait(&cheio);
-		//pthread_mutex_lock(&lock_buffer);
+		pthread_mutex_lock(&lock_buffer);
         processar();
         fflush(stdout);
-        imprime_buffer();
-		//pthread_mutex_unlock(&lock_buffer);
+		pthread_mutex_unlock(&lock_buffer);
 		sem_post(&vazio);
+		//imprime_buffer();
 	}
 }
 
 void processar(){
     int n = 4;
     /// n = 4
+    //imprime_buffer();
     printf("Processando dados....\n");
     int m = in;
     char *temp = (char*)malloc(m*sizeof(char));
+    char *temp2 = (char*)malloc(m*sizeof(char));
     strcpy(temp,buffer);
     strcpy(buffer,"");
     in = 0;
 
     if(m > max_line){
         int teto = m/max_line;
-        printf("M MAIOR QUE N  %d\n", teto);
+        //printf("M MAIOR QUE N  %d\n", teto);
+    }
+    int i,j;
+    for ( i = 0, j = 1; i < m; i++,j++){
+        if(i == max_line) {
+            temp2[i] = '\n';
+            j--;
+        }
+        else if(temp[j] ){
+            temp2[i] = temp[j];
+             }// printf("erro");
+
+    }
+    strcpy(buffer,temp2);
+    //strcpy(buffer,temp);
+    imprime_buffer();
+    //printf("String copiada %s\n", temp);
+    //exit(EXIT_SUCCESS);
+}
+char * le_string(){
+
+    //printf("Lendo entrada\n");
+    char * entrada = (char*)malloc(sizeof(char));
+    it = 1;
+    entrada[0] = '!';
+
+    while(1){
+        scanf("%c", &entrada[it]);
+        if(entrada[it-1] == '.')break;
+        in = (in + 1) % N;
+        it++;
     }
 
 
-    printf("String copiada %s\n", temp);
-    //exit(EXIT_SUCCESS);
+    /* FUNÇÃO DE TESTE
+    while(it<=1) {
+        entrada[it] = '1';
+        it++;
+        in = (in + 1) % N;
+        //usleep(1000000);
+    }
+    */
+    return entrada;
 }
-
 void *leitor() {
-	int item, i=0;
+
+    char * entrada;
 
 	while (1) {
+
+        entrada = le_string();
 		sem_wait(&vazio);
-		//pthread_mutex_lock(&lock_buffer);
-		le_string();
-		fflush(stdout);
-        printf("\nTerminou de ler\n");
-        imprime_buffer();
-		//pthread_mutex_unlock(&lock_buffer);
+		//printf("Lendo..\n");
+		pthread_mutex_lock(&lock_buffer);
+
+        memcpy(buffer, entrada, it);
+        fflush(stdout);
+
+		pthread_mutex_unlock(&lock_buffer);
 		sem_post(&cheio);
+
+        //printf("\nTerminou de ler\n");
+        //imprime_buffer();
 	}
 }
 
-void le_string(){
-    printf("Lendo entrada\n");
-    while(1){
-        scanf("%c", &buffer[in]);
-        if(buffer[in] == '-')return;
-        in = (in + 1) % N;
-    }
-
-}
 
 void imprime_buffer(){
-        printf("BUFFER = %s, IN = %d\n", buffer, in);
+        printf("%s\n", buffer, in);
 }
 
 
 int main()
 {
-    sem_init(&vazio, 0 , N);
+    sem_init(&vazio, 0 , 1);
     sem_init(&cheio, 0 , 0);
     pthread_t t_leitor;
     pthread_t t_processador;
